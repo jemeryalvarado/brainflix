b370740c782409f4f0ef3c0be7101af2cb0666c2
Jemery Laguna Alvarado <jemery@jemerys-macbook.local>
2024-03-18 21:28:38 -0700

    creating styling pages such as mixins, fonts, variables, and applied styling to nav bar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,5 +1,6 @@
 import brainflixlogo from "../../assets/Logo/BrainFlix-logo.svg"
 import avatar from "../../assets/Images/Mohan-muruge.jpg"
+import uploadArrow from '../../assets/Icons/upload.svg'
 import "./navbar.scss"
 
 export default function NavBar() { 
@@ -7,10 +8,12 @@ export default function NavBar() {
         <nav className="navbar">
         <img src= {brainflixlogo} className="navbar-logo">
         </img>
-        <form>
-            <input type= 'text' placeholder="search" className='navbar-search' ></input>
+        <form className="navbar-form">
+            <div className="navbar-search-box">
+            <input type= 'text' placeholder="Search" className='navbar-search' ></input>
         <img src={avatar} alt="avatar" className="navbar-avatar"/>
-            <button className="navbar-button">Upload</button>
+        </div>
+            <button className="navbar-button">UPLOAD</button>
         </form>
         </nav>
 
