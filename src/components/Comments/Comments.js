1413579008674a73200502d1f324aa9407b75851
Jemery Laguna Alvarado <jemery@jemerys-macbook.local>
2024-03-21 18:51:22 -0700

    finished mobile layout

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,6 +2,7 @@ import details from "../../Data/video-details.json";
 import "./Comments.scss";
 import Hero from "../Hero/Hero";
 import comment from "../../assets/Icons/add_comment.svg";
+import avatar from '../../assets/Images/Mohan-muruge.jpg'
 
 export default function Comments({ current, details }) {
   let info = (id) => {
@@ -13,10 +14,13 @@ export default function Comments({ current, details }) {
 
   return (
     <div className="comments">
-      <section className="comment-form">
+      <section className="comments-form">
+        <div className="comments-form-avatar"> <img src={avatar} alt="avatar" /></div>
         <form>
           <h5>Join The Conversation</h5>
-          <input id="commenttext" type="text" placeholder="Add a new comment" />
+
+
+          <input id="commenttext" type="text" placeholder="Add a new comment"></input> 
           <button id="button">
             {" "}
             <img src={comment} alt="upload" />
