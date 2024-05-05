b13994cfc0c3d287c6842955ceb0617e2a6957db
Jemery Laguna Alvarado <jemery@jemerys-macbook.local>
2024-05-04 23:10:25 -0700

    fixing details such as redirect and not being able to enter a value, fixed by putting buttons inside form tag

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -6,41 +6,41 @@ import { useNavigate } from "react-router-dom";
 import publish from "../../assets/Icons/publish.svg";
 
 const VideoUpload = () => {
-  const [formData,setFormData]= useState({
-    title:"",
-    description:"",
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
   });
 
   const goHome = useNavigate();
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    const postData= {
+    const postData = {
       title: formData.title,
       description: formData.description,
-      image: {videoThumbnail}
+      image: videoThumbnail,
     };
 
-    axios.post("http://localhost:8000/videos", postData)
-      .then(response =>{
+    axios
+      .post("http://localhost:8000/videos", postData)
+      .then((response) => {
         setFormData({
-          title:"",
+          title: "",
           description: "",
         });
-        
-        goHome('/')
 
+        goHome("/");
       })
-      .catch(error =>{
-        console.log('error uploading video:', error)
+      .catch((error) => {
+        console.log("error uploading video:", error);
       });
   };
 
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -48,51 +48,50 @@ const VideoUpload = () => {
     <div className="video-upload">
       <h2 className="video-upload__title">Upload Video</h2>
       <section className="video-thumbnail-form__wrapper">
-      <div className="video-upload__thumbnail">
-        <h3 className="video-upload__thumbnail-title">VIDEO THUMBNAIL</h3>
-        <img
-          src={videoThumbnail}
-          alt="Video Thumbnail"
-          className="video-upload__thumbnail-image"
-        />
-      </div>
-      <form className="video-upload__form">
-        <div className="form-group">
-          <label className="form-label">TITLE YOUR VIDEO</label>
-          <input
-            type="text"
-            id="video-title"
-            name="video-title"
-            placeholder="Add a title to your video"
-            value={formData.title}
-            onChange={handleChange}
-            required
-          ></input>
+        <div className="video-upload__thumbnail">
+          <h3 className="video-upload__thumbnail-title">VIDEO THUMBNAIL</h3>
+          <img
+            src={videoThumbnail}
+            alt="Video Thumbnail"
+            className="video-upload__thumbnail-image"
+          />
         </div>
-        <div className="form-group">
-          <label className="form-label">ADD A VIDEO DESCRIPTION</label>
-          <input
-            type="text"
-            id="video-description"
-            name="video-description"
-            placeholder="Add a description to your video"
-            value={formData.description}
-            onChange={handleChange}
-            required
-          ></input>
-        </div>
-      </form>
+        <form className="video-upload__form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label className="form-label">TITLE YOUR VIDEO</label>
+            <input
+              type="text"
+              id="video-title"
+              name="title"
+              placeholder="Add a title to your video"
+              value={formData.title}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className="form-group">
+            <label className="form-label">ADD A VIDEO DESCRIPTION</label>
+            <input
+              type="text"
+              id="video-description"
+              name="description"
+              placeholder="Add a description to your video"
+              value={formData.description}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className="btns">
+            <button type="submit" className="btn btn-primary">
+              <img className="icon" src={publish} alt="Upload" />
+              <p className="btn-primary-name">Publish</p>
+            </button>
+            <button type="button" className="btn btn-secondary">
+              Cancel
+            </button>
+          </div>
+        </form>
       </section>
-      <div className="btns">
-        <button type="submit" className="btn btn-primary">
-          <img class="icon" src={publish} alt="Upload" />
-          <p className="btn-primary-name">Publish</p>
-        </button>
-
-        <button type="button" className="btn btn-secondary">
-          Cancel
-        </button>
-      </div>
     </div>
   );
 };
