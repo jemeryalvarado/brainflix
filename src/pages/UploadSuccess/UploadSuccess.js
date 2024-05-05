2b3713c673a05a16d0ee3e4915a66523d67568d8
Jemery Laguna Alvarado <jemery@jemerys-macbook.local>
2024-05-05 08:57:00 -0700

    making sure everything us up to date

---
+++ src/pages/UploadSuccess/UploadSuccess.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Description from "../../components/Description/Description.js";
+import Videos from "../../components/Videos/Videos.js";
+import Comments from "../../components/Comments/Comments.js";
+import "./UploadSuccess.scss"
+
