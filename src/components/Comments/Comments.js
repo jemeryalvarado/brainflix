50c8950908c052bbbeedf3eb7e658ae973d6288c
Jemery Laguna Alvarado <jemery@jemerys-macbook.local>
2024-05-05 08:49:48 -0700

    finishing video upload post reuqest to add new video

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,12 +1,10 @@
-import details from "../../Data/video-details.json";
 import "./Comments.scss";
-import Hero from "../Hero/Hero";
 import comment from "../../assets/Icons/add_comment.svg";
 import avatar from '../../assets/Images/Mohan-muruge.jpg'
 
 
 
-const Comments = ({ current, details }) => {
+const Comments = ({ details }) => {
   const display = details;
   const displayComments = display.comments;
 
@@ -52,82 +50,3 @@ const Comments = ({ current, details }) => {
 
 export default Comments;
 
-// export default function Comments({ current, details }) {
-//   let info = (id) => {
-//     return details.find((item) => item.id === id);
-//   };
-//   const display = info(current);
-
-//   let displayComments = display.comments;
-
-//   const formatDate = (timestamp) => {
-//     const date = new Date(timestamp);
-//     const month = date.getMonth() + 1;
-//     const day = date.getDate();
-//     const year = date.getFullYear();
-//     return `${month}/${day}/${year}`;
-//   };
-
-//   return (
-    
-
-//     <div className="comments">
-
-
-
-
-//       <section className="comments-form">
-
-
-
-
-//         <div className="comments-form-avatar"> <img src={avatar} alt="avatar" /></div>
-//         <form>
-//           <h5>JOIN THE CONVERSATION</h5>
-
-
-//           <input id="commenttext" type="text" placeholder="Add a new comment"></input> 
-//           <button id="button">
-//             {" "}
-//             <img src={comment} alt="upload" />
-//             COMMENT
-//           </button>
-//         </form>
-
-       
-
-
-
-
-
-
-
-
-
-//       </section>
-
-//       <div className="comments-form-break"></div>
-
-
-//       {displayComments.map((comment) => (
-        
-//         <>
-        
-//           <section className="comments-text">
-//             <div className="comments-text-avblank"></div>
-//             <div className="comments-text-name" key={comment.id}>{comment.name} {formatDate(comment.timestamp)}</div>
-//             {/* <div className="comments-text-time">  </div> */}
-
-//             <div className="comments-text-words" >{comment.comment}</div>
-//           </section>
-//         </>
-//       ))}
-
-
-
-
-
-
-//     </div>
-//   );
-// }
