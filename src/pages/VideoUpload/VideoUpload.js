50545dfb10103fd27ea4c96d0c71bc03d5ebc120
Jemery Laguna Alvarado <jemery@jemerys-macbook.attlocal.net>
2024-05-04 17:50:56 -0700

    adding tablet media queries and adjusting styling for tablet view

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -1,9 +1,9 @@
 import "./VideoUpload.scss";
-import videoThumbnail from '../../assets/Images/Upload-video-preview.jpg'
+import videoThumbnail from "../../assets/Images/Upload-video-preview.jpg";
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import publish from "../../assets/Icons/publish.svg"
+import publish from "../../assets/Icons/publish.svg";
 
 const VideoUpload = () => {
   return (
@@ -19,41 +19,35 @@ const VideoUpload = () => {
       </div>
       <form className="video-upload__form">
         <div className="form-group">
-          <label  className="form-label">
-            TITLE YOUR VIDEO
-          </label>
+          <label className="form-label">TITLE YOUR VIDEO</label>
           <input
-          type="text"
+            type="text"
             id="video-title"
             name="video-title"
             placeholder="Add a title to your video"
           ></input>
         </div>
         <div className="form-group">
-          <label  className="form-label">
-            ADD A VIDEO DESCRIPTION
-          </label>
+          <label className="form-label">ADD A VIDEO DESCRIPTION</label>
           <input
             id="video-description"
             name="video-description"
             placeholder="Add a description to your video"
           ></input>
         </div>
-        <div className="btns">
-
+      </form>
+      <div className="btns">
         <button type="submit" className="btn btn-primary">
-        <img class="icon" src={publish} alt="Upload"/>
-         <p className="btn-primary-name">Publish</p> 
+          <img class="icon" src={publish} alt="Upload" />
+          <p className="btn-primary-name">Publish</p>
         </button>
 
         <button type="button" className="btn btn-secondary">
           Cancel
         </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
 
 export default VideoUpload;
-
